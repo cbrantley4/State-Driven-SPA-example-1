95291ddde2e8fa008e28ec8a5ee0b294a3cc83ce
Johan Bester <johan.bester@yahoo.com>
2021-08-01 12:23:33 -0500

    Update State Driven SPA Example

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,6 +3,8 @@ import html from "html-literal";
 export default () => html`
   <section id="jumbotron">
     <h2>Savvy Coders Jan. 2020 Cohort</h2>
-    <a href="">"Call to Action" "Button"</a>
+    <a href="" onClick="alert('Hello! You clicked the Button!')"
+      >"Call to Action" "Button"</a
+    >
   </section>
 `;
